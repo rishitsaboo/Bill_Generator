7b78bc5f9309dc060d56a11e5998796e0e4ab951
rishitsaboo <saboorishit27@gmail.com>
2026-03-17 14:01:42 +0530

    fixed issue related env

--- Backend/my_app/routes/itemRoutes.js
+++ Backend/my_app/routes/itemRoutes.js
@@ -3,25 +3,23 @@ const router = express.Router();
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('../config/cloudinary');
-const Item = require('../models/ItemModel')
-const itemController = require('../controllers/itemControllers')
+const itemController = require('../controllers/itemControllers');
 
 const storage = new CloudinaryStorage({
-    cloudinary : cloudinary,
-    params: {
-        folder: 'bill_itmes',
-        allowed_formats: ['jpg','jpeg', 'png']
-    }
+  cloudinary: cloudinary,
+  params: {
+    folder: 'bill_items',
+    allowed_formats: ['jpg', 'jpeg', 'png']
+  }
 });
 
 const upload = multer({ storage: storage });
 
-// item schema
+// ROUTES
 router.get('/items/category/:categoryName', itemController.getItemsByCategory);
-router.post('/add-item',upload.single('image'),itemController.addItem);
+router.post('/add-item', upload.single('image'), itemController.addItem);
 router.delete('/delete-item/:id', itemController.deleteItem);
 router.put('/update-price/:id', itemController.updatePrice);
 router.get('/items', itemController.getAllItems);
-
 
 module.exports = router;
